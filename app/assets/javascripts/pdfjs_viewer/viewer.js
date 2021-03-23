c093583baffa60416682f17f74165f01be3da962
Mark Kenny <markmkenny@gmail.com>
2021-03-23 15:21:28 +0000

    changed origin host

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1886,7 +1886,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'https://*.beebop-staging.app', 'https://*.beebop.app'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
