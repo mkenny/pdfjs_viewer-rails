4a539d70bd34553ce404045718a95c9ffcd108f8
Mark Kenny <markmkenny@gmail.com>
2021-03-23 15:32:53 +0000

    added localhost

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1886,7 +1886,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'https://*.beebop-staging.app', 'https://*.beebop.app'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'http://*.virtual.local:3000', 'https://*.beebop-staging.app', 'https://*.beebop.app'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
